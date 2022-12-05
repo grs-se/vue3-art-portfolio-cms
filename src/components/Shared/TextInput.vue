40a104c8bde00d2dfcc7a21b28ed58e197812275
grs-se <george_ricesmith@hotmail.com>
2022-12-05 09:31:05 +0000

    Updated SearchForm and TextInput with v-model directive

--- src/components/Shared/TextInput.vue
+++ src/components/Shared/TextInput.vue
@@ -1,42 +1,48 @@
 <template>
-  <input
-    type="text"
-    :value="value"
-    class="text-md h-full w-full font-normal focus:outline-none"
-    :placeholder="placeholder"
-    @input="handleInput"
-  />
+	<input
+		type="text"
+		:value="modelValue"
+		class="text-md h-full w-full font-normal focus:outline-none"
+		:placeholder="placeholder"
+		@input="handleInput"
+	/>
 </template>
 
 <script>
 import nextElementInList from "@/utils/nextElementInList";
 
 export default {
-  name: "TextInput",
-  data() {
-    return {
-      value: "",
-      placeholder: "Painting",
-      interval: null,
-    };
-  },
-  created() {
-    this.changePlaceholder();
-  },
-  beforeUnmount() {
-    clearInterval(this.interval);
-  },
-  methods: {
-    changePlaceholder() {
-      this.interval = setInterval(() => {
-        const placeholders = ["Painting", "Winchester", "2022", "Imagination"];
-        this.placeholder = nextElementInList(placeholders, this.placeholder);
-      }, 3000);
-    },
-    handleInput($event) {
-      this.value = $event.target.value;
-      this.$emit("handleInput", this.value);
-    },
-  },
+	name: "TextInput",
+	props: {
+		modelValue: {
+			type: String,
+			required: true,
+		},
+	},
+	emits: ["update:modelValue"],
+	data() {
+		return {
+			value: "",
+			placeholder: "Painting",
+			interval: null,
+		};
+	},
+	created() {
+		this.changePlaceholder();
+	},
+	beforeUnmount() {
+		clearInterval(this.interval);
+	},
+	methods: {
+		changePlaceholder() {
+			this.interval = setInterval(() => {
+				const placeholders = ["Painting", "Winchester", "2022", "Imagination"];
+				this.placeholder = nextElementInList(placeholders, this.placeholder);
+			}, 3000);
+		},
+		handleInput($event) {
+			this.$emit("update:modelValue", $event.target.value);
+		},
+	},
 };
 </script>
