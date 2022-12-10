3c830c15327c4f57551e16a1a87ac6ac188ff8bc
grs-se <george_ricesmith@hotmail.com>
2022-12-10 15:31:23 +0000

    Create named slot for reuseable Header

--- src/views/ArtistStatementView.vue
+++ src/views/ArtistStatementView.vue
@@ -1,2 +1,16 @@
-<template><div class=""></div></template>
-<script></script>
+<template>
+	<header-container>
+		<template v-slot:title>
+			<h1 class="w-full text-4xl font-normal">Artist Statement</h1>
+		</template>
+		<template v-slot:subtitle>
+			<h2 class="my-4 w-full text-base font-light">2022-2023</h2>
+		</template>
+	</header-container>
+</template>
+
+<script>
+import HeaderContainer from "@/components/Shared/HeaderContainer.vue";
+
+export default { name: "ArtistStatementView", components: { HeaderContainer } };
+</script>
