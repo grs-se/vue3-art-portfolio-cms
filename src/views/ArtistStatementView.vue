21eaa0e3f572212f16c06ed5031e58e67f5adc81
grs-se <george_ricesmith@hotmail.com>
2022-12-10 18:49:59 +0000

    Add named slots to ArtistStatement header-container

--- src/views/ArtistStatementView.vue
+++ src/views/ArtistStatementView.vue
@@ -1,9 +1,9 @@
 <template>
 	<header-container>
-		<template v-slot:title>
+		<template #title>
 			<h1 class="w-full text-4xl font-normal">Artist Statement</h1>
 		</template>
-		<template v-slot:subtitle>
+		<template #subtitle>
 			<h2 class="my-4 w-full text-base font-light">2022-2023</h2>
 		</template>
 	</header-container>
