feae8f8da8c7aba6343d70872fde2c90c62162fe
grs-se <george_ricesmith@hotmail.com>
2022-12-12 17:40:04 +0000

    WIP FILTERED_ARTWORKS_BY_CATEGORIES

--- src/utils/createSet.js
+++ src/utils/createSet.js
@@ -1,10 +1,8 @@
 const createSet = (data, key) => {
 	const arr = [];
 
-	data.forEach((object) =>
-		typeof object[key] === "string"
-			? arr.push(object[key])
-			: arr.push(...object[key])
+	data.forEach((item) =>
+		typeof item[key] === "string" ? arr.push(item[key]) : arr.push(...item[key])
 	);
 	const set = new Set(arr);
 	// console.log(set);
