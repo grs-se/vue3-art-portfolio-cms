4a10e2188a649490fc57d65bd607dac71c027f57
grs-se <george_ricesmith@hotmail.com>
2022-12-14 11:13:06 +0000

    Implement FILTER_ARTWORKS_BY_CATEGORIES getter method

--- src/stores/artworks.js
+++ src/stores/artworks.js
@@ -24,8 +24,7 @@ export const useArtworksStore = defineStore("artworks", {
 		[UNIQUE_CATEGORIES](state) {
 			return createSet(state.artworks, "categories");
 		},
-		// Filter state.artworks based on user's selectedCaterories.
-		// Loop through an array of categories inside array fo artworks
+
 		[FILTERED_ARTWORKS_BY_CATEGORIES](state) {
 			const userStore = useUserStore();
 
@@ -33,135 +32,13 @@ export const useArtworksStore = defineStore("artworks", {
 				return state.artworks;
 			}
 
-			// console.log(userStore.selectedCategories);
+			console.log(userStore.selectedCategories);
 
-			/*
-			return state.artworks.filter((artwork) => {
-				const returnArrayCategories = function (data) {
-					data.categories.forEach((el) => {
-						if (userStore.selectedCategories.includes(el)) {
-							// console.log(artwork.categories);
-							console.log(el);
-							// console.log(data);
-							return data;
-						}
-					});
-				};
-				userStore.selectedCategories.includes(returnArrayCategories(artwork));
-			});
-			// */
-			/*
-			return state.artworks.filter((artwork) => {
-				artwork.categories.forEach((el) => {
-					if (userStore.selectedCategories.includes(el)) {
-						console.log(el);
-						return el;
-					}
-				});
-				return artwork;
-			});
-			// */
-			// /*
-
-			const returnArrayCategories = function (data) {
-				data.categories.forEach((el) => {
-					if (userStore.selectedCategories.includes(el)) {
-						// console.log(data.categories);
-						console.log(data);
-						console.log(el);
-						return el, data;
-					}
-				});
-			};
-
-			return state.artworks.filter((artwork) => {
-				userStore.selectedCategories.includes(returnArrayCategories(artwork));
-				console.log();
-			});
-
-			// */
-
-			/*
-			return state.artworks.filter((artwork) => {
-				artwork.categories.forEach((el) => {
-					if (userStore.selectedCategories.includes(el)) {
-						// console.log(artwork.categories);
-						console.log(artwork);
-						return artwork;
-					}
-				});
-			});
-			// */
-
-			// /*
-			// return state.artworks.filter((artwork) => {
-			// 	userStore.selectedCategories.includes(artwork.categories);
-			// 	console.log(artwork.categories);
-			// });
-			// /*
-
-			/*
-			return state.artworks.filter((artwork) => {
-				userStore.selectedCategories.includes(
-					artwork.categories.forEach((el) => {
-						console.log(el);
-						return el;
-					})
-				);
-			});
-			*/
-
-			// const artworksFilter = state.artworks.filter((artwork) => {
-			// 	artwork.categories;
-			// });
-
-			// const handleCallback = (childData) => {
-			// 	state.artworks.filter((artwork) =>
-			// 		userStore.selectedCategories.includes()
-			// 	);
-			// };
-
-			// const parentCallback = () => {};
-
-			// state.artworks.
-
-			// const filter = (data1, data2) => {data1.filter((obj) => {
-			// 	obj.
-			// })}
-
-			// if (userStore.selectedCategories.includes(state.artworks.categories))
-			// 	return;
-
-			// if (state.artworks.includes(userStore.selectedCategories)) {
-			// 	return state.artworks;
-			// }
-
-			// return state.artworks.filter((artwork) => {
-			// 	// artwork.categories.includes(userStore.selectedCategories);
-			// 	userStore.selectedCategories.includes(
-			// 		artwork.categories.some((i) => i)
-			// 	);
-			// });
-
-			// return state.artworks.filter((artwork) => {
-			// 	const categories = artwork.categories.forEach((el) => console.log(el));
-			// 	userStore.selectedCategories.includes(categories);
-			// 	console.log(categories);
-			// });
-
-			// userStore.selectedCategories.includes(categories);
-
-			// return state.artworks.filter((artwork) => {
-			// 	userStore.selectedCategories.includes(
-			// 		artwork.categories.forEach((cat) => {
-			// 			console.log(cat);
-			// 			return cat;
-			// 		})
-			// 	);
-
-			// const cats = [...artwork.categories];
-			// console.log(cats);
-			// userStore.selectedCategories.includes(cats);
+			return state.artworks.filter((artwork) =>
+				artwork.categories.some((cat) =>
+					userStore.selectedCategories.includes(cat)
+				)
+			);
 		},
 	},
 });
