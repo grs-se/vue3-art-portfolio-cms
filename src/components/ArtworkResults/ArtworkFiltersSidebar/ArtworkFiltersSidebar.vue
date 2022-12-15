564f8260cac3f142b311622a448d005b62307fe0
grs-se <george_ricesmith@hotmail.com>
2022-12-15 15:07:28 +0000

    Update ArtworkFiltersSidebar

--- src/components/ArtworkResults/ArtworkFiltersSidebar/ArtworkFiltersSidebar.vue
+++ src/components/ArtworkResults/ArtworkFiltersSidebar/ArtworkFiltersSidebar.vue
@@ -3,12 +3,12 @@
 		class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
 	>
 		<section class="pb-5">
-			<div class="flex flex-row justify-between">
+			<!-- <div class="flex flex-row justify-between">
 				<h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
 				<div class="flex items-center text-sm">
 					<action-button text="Clear Filters" type="secondary" />
 				</div>
-			</div>
+			</div> -->
 			<artwork-filters-sidebar-categories />
 			<artwork-filters-sidebar-locations />
 			<collapsible-accordian header="Medium">
