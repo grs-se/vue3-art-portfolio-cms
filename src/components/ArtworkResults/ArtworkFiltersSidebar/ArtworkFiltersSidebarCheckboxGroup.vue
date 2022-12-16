4bc2289faf4e25a0a3659855a1f89be8a3ff3d18
grs-se <george_ricesmith@hotmail.com>
2022-12-16 15:53:04 +0000

    Create ArtworkFiltersSidebarCheckboxGroup component

---
+++ src/components/ArtworkResults/ArtworkFiltersSidebar/ArtworkFiltersSidebarCheckboxGroup.vue
@@ -0,0 +1,51 @@
+<template>
+	<collapsible-accordian :header="header">
+		<div class="mt-5">
+			<fieldset>
+				<ul class="flex flex-row flex-wrap">
+					<li v-for="value in uniqueValues" :key="value" class="h-8 w-1/2">
+						<input
+							:id="value"
+							v-model="selectedValues"
+							:value="value"
+							type="checkbox"
+							class="mr-3"
+							@change="selectValue"
+						/>
+						<label :for="value">{{ value }}</label>
+					</li>
+				</ul>
+			</fieldset>
+		</div>
+	</collapsible-accordian>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+import CollapsibleAccordian from "@/components/Shared/CollapsibleAccordian.vue";
+
+const props = defineProps({
+	header: {
+		type: String,
+		required: true,
+	},
+	uniqueValues: {
+		type: Set,
+		required: true,
+	},
+	action: {
+		type: Function,
+		required: true,
+	},
+});
+
+const selectedValues = ref([]);
+const router = useRouter();
+
+const selectValue = () => {
+	props.action(selectedValues.value);
+	router.push({ name: "ArtworkResults" });
+};
+</script>
---
+++ tests/unit/components/ArtworkResults/ArtworkFiltersSidebar/ArtworkFiltersSidebarCheckboxGroup.test.js
@@ -0,0 +1,89 @@
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+import { createTestingPinia } from "@pinia/testing";
+
+import { useRouter } from "vue-router";
+vi.mock("vue-router");
+
+import ArtworkFiltersSidebarCheckboxGroup from "@/components/ArtworkResults/ArtworkFiltersSidebar/ArtworkFiltersSidebarCheckboxGroup.vue";
+import { vi } from "vitest";
+
+describe("ArtworkFiltersSidebarCheckboxGroup", () => {
+	const createProps = (props = {}) => ({
+		header: "Some header",
+		uniqueValues: new Set(["ValA", "ValB"]),
+		action: vi.fn(),
+		...props,
+	});
+
+	const renderArtworkFilterSidebarCheckboxGroup = (props) => {
+		const pinia = createTestingPinia();
+
+		render(ArtworkFiltersSidebarCheckboxGroup, {
+			props: { ...props },
+			global: {
+				plugins: [pinia],
+				stubs: {
+					FontAwesomeIcon: true,
+				},
+			},
+		});
+	};
+
+	it("renders unique list of values", async () => {
+		const props = createProps({
+			header: "Categories",
+			uniqueValues: new Set(["Painting", "Studio"]),
+		});
+		renderArtworkFilterSidebarCheckboxGroup(props);
+
+		const button = screen.getByRole("button", { name: /categories/i });
+		await userEvent.click(button);
+
+		const categoriesListItems = screen.getAllByRole("listitem");
+		const categories = categoriesListItems.map((node) => node.textContent);
+		expect(categories).toEqual(["Painting", "Studio"]);
+	});
+
+	describe("when user clicks checkbox", () => {
+		it("communicates that user has selected checkbox for categories", async () => {
+			useRouter.mockReturnValue({ push: vi.fn() });
+			const action = vi.fn();
+			const props = createProps({
+				header: "Categories",
+				uniqueValues: new Set(["Painting", "Studio"]),
+				action,
+			});
+			renderArtworkFilterSidebarCheckboxGroup(props);
+
+			const button = screen.getByRole("button", { name: /categories/i });
+			await userEvent.click(button);
+
+			const categoriesCheckbox = screen.getByRole("checkbox", {
+				name: /painting/i,
+			});
+			await userEvent.click(categoriesCheckbox);
+
+			expect(action).toHaveBeenCalledWith(["Painting"]);
+		});
+
+		it("navigates user to artwork results page to see fresh batch of filtered artworks", async () => {
+			const push = vi.fn();
+			useRouter.mockReturnValue({ push });
+			const props = createProps({
+				header: "Categories",
+				uniqueValues: new Set(["Painting"]),
+			});
+			renderArtworkFilterSidebarCheckboxGroup(props);
+			const button = screen.getByRole("button", { name: /categories/i });
+			await userEvent.click(button);
+
+			const categoriesCheckbox = screen.getByRole("checkbox", {
+				name: /painting/i,
+			});
+			await userEvent.click(categoriesCheckbox);
+
+			expect(push).toHaveBeenCalledWith({ name: "ArtworkResults" });
+		});
+	});
+});
