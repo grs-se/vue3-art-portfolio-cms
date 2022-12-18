be45edc43b280b5348b669623d8710a63ab3b710
grs-se <george_ricesmith@hotmail.com>
2022-12-18 13:16:36 +0000

    Create BlogPosts component

---
+++ src/components/BlogPosts/BlogPosts.vue
@@ -0,0 +1,16 @@
+<template>
+	<section>
+		<header-container
+			><template #title>
+				<h1 class="w-full text-4xl font-normal">Blog</h1>
+			</template>
+			<template #subtitle>
+				<h2 class="my-4 w-full text-base font-light">Recent Posts</h2>
+			</template></header-container
+		>
+	</section>
+</template>
+
+<script setup>
+import HeaderContainer from "@/components/Shared/HeaderContainer.vue";
+</script>
