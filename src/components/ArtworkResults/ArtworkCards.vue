1b264b2e2ee4b17cc508c8ce5b2a8042958ac02e
grs-se <george_ricesmith@hotmail.com>
2022-12-18 13:14:49 +0000

    Add borders to Grid Cards

---
+++ src/components/ArtworkResults/ArtworkCards.vue
@@ -0,0 +1,24 @@
+<template>
+	<ol>
+		<li v-for="artwork in FILTERED_ARTWORKS" :key="artwork._id" class="">
+			<slot
+				:image-cover="artwork.imageCover"
+				:title="artwork.title"
+				:medium="artwork.medium"
+				:dimensions="artwork.dimensions"
+				:year="artwork.year"
+			></slot>
+		</li>
+	</ol>
+</template>
+
+<script setup>
+import { useArtworksStore } from "@/stores/artworks";
+
+import { computed, onMounted } from "vue";
+
+const artworksStore = useArtworksStore();
+onMounted(artworksStore.FETCH_ARTWORKS);
+
+const FILTERED_ARTWORKS = computed(() => artworksStore.FILTERED_ARTWORKS);
+</script>
--- src/components/ArtworkResults/ArtworkGalleries/GridCardsGallery.vue
+++ src/components/ArtworkResults/ArtworkGalleries/GridCardsGallery.vue
@@ -1,35 +1,42 @@
 <template>
-	<artwork-card
+	<section id="grid-cards-gallery" class="bg-brand-grey-1">
+		<artwork-cards
+			class="mx-auto grid border-l border-t sm:grid-cols-1 md:grid-cols-2 xl:max-w-gallery xl:grid-cols-3"
+		>
+			<!-- <artwork-card
 		class="mx-auto grid rounded border border-solid bg-white p-4 sm:grid-cols-1 sm:gap-2 md:grid-cols-2 xl:max-w-gallery xl:grid-cols-3 xl:gap-9"
-	>
-		<template #default="{ imageCover, title }">
-			<figure class="flex h-full w-full">
-				<!-- <figure
+	> -->
+			<template #default="{ imageCover, title }">
+				<figure
+					class="flex h-full w-full border-b border-r border-solid border-brand-gray-1 p-4 hover:bg-white"
+				>
+					<!-- <figure
 				class="flex h-full w-full rounded border border-solid border-brand-gray-2 hover:shadow-gray"
 				
 			> -->
-				<img
-					:src="'/images/artworks/' + imageCover"
-					class="mx-auto max-h-60 place-self-center hover:shadow-gray"
-					@mouseover="hover = true"
-					@mouseleave="hover = false"
-				/>
-			</figure>
-			<figcaption class="hidden bg-white">
-				<h3>{{ title }}</h3>
-			</figcaption>
-		</template>
-	</artwork-card>
+					<img
+						:src="'/images/artworks/' + imageCover"
+						class="mx-auto max-h-60 place-self-center hover:shadow-gray"
+						@mouseover="hover = true"
+						@mouseleave="hover = false"
+					/>
+				</figure>
+				<figcaption class="hidden bg-white">
+					<h3>{{ title }}</h3>
+				</figcaption>
+			</template>
+		</artwork-cards>
+	</section>
 </template>
 
 <script>
-import ArtworkCard from "@/components/ArtworkResults/ArtworkCard.vue";
+import ArtworkCards from "@/components/ArtworkResults/ArtworkCards.vue";
 import ArtworkCardHoverModal from "@/components/ArtworkResults/ArtworkCardHoverModal.vue";
 
 export default {
 	name: "GridCardsGallery",
 	components: {
-		ArtworkCard,
+		ArtworkCards,
 		ArtworkCardHoverModal,
 	},
 	data() {
