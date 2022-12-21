4fb06f55d8a2af7fec8f050c12e151503726c989
grs-se <george_ricesmith@hotmail.com>
2022-12-21 20:07:19 +0000

    Attempt implement pinia for texts and getter functions using Composition API syntax

---
+++ src/stores/texts2.ts
@@ -0,0 +1,42 @@
+import { computed, ref } from "vue";
+import { defineStore } from "pinia";
+
+import { useUserStore } from "@/stores/user";
+
+import createSet from "@/utils/createSetFromNestedArray.js";
+
+import getTexts from "@/api/getTexts";
+import type { Text } from "@/api/types";
+
+export const useTextsStore = defineStore("texts", () => {
+	const texts = ref<Text[]>([]);
+
+	const FETCH_TEXTS = async () => {
+		const receivedTexts = await getTexts();
+		texts.value = receivedTexts;
+	};
+
+	const UNIQUE_TEXT_CATEGORIES = computed(() => {
+		return createSet(texts.value, "categories");
+	});
+
+	const INCLUDE_TEXT_BY_CATEGORY = computed((text: Text) => {
+		const userStore = useUserStore();
+		if (userStore.selectedTextCategories.length === 0) return true;
+		return text.categories.some((cat) =>
+			userStore.selectedTextCategories.includes(cat)
+		);
+	});
+
+	const FILTERED_TEXTS = computed((texts) => {
+		return texts.value.filter((text) => INCLUDE_TEXT_BY_CATEGORY(text));
+	});
+
+	return {
+		texts,
+		FETCH_TEXTS,
+		UNIQUE_TEXT_CATEGORIES,
+		INCLUDE_TEXT_BY_CATEGORY,
+		FILTERED_TEXTS,
+	};
+});
