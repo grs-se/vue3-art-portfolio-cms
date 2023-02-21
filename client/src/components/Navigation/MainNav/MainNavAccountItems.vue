52d9e547541d24d368d1820028592e60c9eaf4ef
grs-se <george_ricesmith@hotmail.com>
2023-02-21 11:11:27 +0000

    Temporary styling for LoginModal placement

--- client/src/components/Navigation/MainNav/MainNavAccountItems.vue
+++ client/src/components/Navigation/MainNav/MainNavAccountItems.vue
@@ -1,22 +1,33 @@
 <script lang="ts" setup>
+import { ref } from "vue";
 import { storeToRefs } from "pinia";
 import { useAuthStore } from "@/stores";
 
 import { ActionButton } from "@/components/Shared";
 import Avatar from "./MainNavAvatar.vue";
 
+import { LoginForm } from "@/components/auth/Login";
+
 const authStore = useAuthStore();
 const { user } = storeToRefs(authStore);
 
-const toggleLoginModal = () => {};
+const modalOpen = ref(false);
+
+const toggleLoginModal = () => {
+	modalOpen.value = !modalOpen.value;
+	console.log(modalOpen.value);
+};
 </script>
 
 <template>
 	<div class="ml-auto flex h-full items-center">
 		<Avatar v-if="user" />
-		<router-link v-else to="/LoginRegister">
-			<!-- <ActionButton text="Sign in" @click="login" /> -->
-			<ActionButton text="Sign in" @click="toggleLoginModal" />
-		</router-link>
+		<!-- <router-link v-else to="/LoginRegister"> -->
+		<!-- <ActionButton text="Sign in" @click="login" /> -->
+		<ActionButton text="Sign in" @click="toggleLoginModal" />
+		<!-- </router-link> -->
+		<!-- <div class="relative"> -->
+		<LoginForm v-if="modalOpen === true" class="absolute top-48 right-1/3" />
+		<!-- </div> -->
 	</div>
 </template>
